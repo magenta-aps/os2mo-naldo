c4b99c41504b264610d9309abbf429802c579856
Andreas Brodersen <apb@magenta-aps.dk>
2022-08-23 10:58:27 +0200

    feat: add keycloak + custom fetch with bearer + refactor

--- web/src/routes/organisation/[uuid].svelte
+++ web/src/routes/organisation/[uuid].svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-	import { page } from '$app/stores'
-	import OrgTable from '$lib/components/org/org_table.svelte'
+  import { page } from "$app/stores"
+  import OrgTable from "$lib/components/org/org_table.svelte"
+  import { fetchGraph } from "$lib/util/http"
 
-	let currentTab = 0
-	const menuItems = [
-		'Enhed',
-		'Adressser',
-		'Engagementer',
-		'Tilknytninger',
-		'IT',
-		'Roller',
-		'Ledere',
-		'KLE-opmærkninger',
-		'Relateret'
-	]
+  let currentTab = 0
+  const menuItems = [
+    "Enhed",
+    "Adressser",
+    "Engagementer",
+    "Tilknytninger",
+    "IT",
+    "Roller",
+    "Ledere",
+    "KLE-opmærkninger",
+    "Relateret",
+  ]
 
-	$: uuid = $page.params.uuid
+  $: uuid = $page.params.uuid
 
-	$: query = `
+  $: query = `
     query {
       org_units(uuids: "${uuid}") {
         uuid
@@ -43,84 +44,63 @@
       }
     }
   `
-	const fetchOrgGraph = async (query: string) => {
-		const url = 'http://localhost:5000/graphql'
-		const res = await fetch(url, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				query: query
-			})
-		})
 
-		const response = await res.json()
-		if (response.data) {
-			return response.data.org_units[0].objects[0]
-		} else {
-			throw new Error(response.errors[0].message)
-		}
-	}
-
-	// Using service API
-	// const fetchOrgDetails = async (uuid: string) => {
-	// 	const url = `http://localhost:5000/service/ou/${uuid}/details/org_unit`
-	// 	const response = await fetch(url)
-
-	// 	if (response.status == 200) {
-	// 		return await response.json()
-	// 	} else {
-	// 		const res = await response.json()
-	// 		throw new Error(res.description)
-	// 	}
-	// }
+  const fetchOrgGraph = async (query: string) => {
+    const res = await fetchGraph(query)
+    const json = await res.json()
+    if (json.data) {
+      return json.data.org_units[0].objects[0]
+    } else {
+      throw new Error(json.errors[0].message)
+    }
+  }
 </script>
 
 {#await fetchOrgGraph(query)}
-	<div class="m-auto">
-		<div class="animate-spin rounded-full h-32 w-32 border-b-8 border-primary" />
-	</div>
+  <div class="m-auto">
+    <div class="animate-spin rounded-full h-32 w-32 border-b-8 border-primary" />
+  </div>
 {:then org}
-	<div class="flex justify-center pt-10">
-		<div class="hero bg-base-200">
-			<div class="hero-content text-center">
-				<div>
-					<h1 class="text-3xl font-bold pb-1">{org.name}</h1>
-					<h1 class="text-sm pb-5">
-						{#if org.parent.length}
-							<i>Placering: {org.parent[0].name}</i>
-						{/if}
-					</h1>
-					<div class="tabs tabs-boxed bg-base-300 flex justify-center">
-						{#each menuItems as menuItem, index}
-							{#if index == currentTab}
-								<a href="" class="tab tab-boxed tab-active">{menuItem}</a>
-							{:else}
-								<a href="" on:click={() => (currentTab = index)} class="tab tab-boxed">{menuItem}</a
-								>
-							{/if}
-						{/each}
-					</div>
-					<div class="py-5">
-						<h1>Enhed: {org.name}</h1>
-						<h1>Enhedstype: {org.unit_type.name}</h1>
-						<h1>Enhedsniveu: {org.org_unit_level.name}</h1>
-						<h1>
-							Gyldig fra: {new Date(org.validity.from).toUTCString()}
-						</h1>
-						<h1>
-							Gyldig til: {org.validity.to != 'null'
-								? 'Ingen'
-								: new Date(org.validity.from).toUTCString()}
-						</h1>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="flex justify-center pt-10">
+    <div class="hero bg-base-200">
+      <div class="hero-content text-center">
+        <div>
+          <h1 class="text-3xl font-bold pb-1">{org.name}</h1>
+          <h1 class="text-sm pb-5">
+            {#if org.parent.length}
+              <i>Placering: {org.parent[0].name}</i>
+            {/if}
+          </h1>
+          <div class="tabs tabs-boxed bg-base-300 flex justify-center">
+            {#each menuItems as menuItem, index}
+              {#if index == currentTab}
+                <a href="" class="tab tab-boxed tab-active">{menuItem}</a>
+              {:else}
+                <a href="" on:click={() => (currentTab = index)} class="tab tab-boxed"
+                  >{menuItem}</a
+                >
+              {/if}
+            {/each}
+          </div>
+          <div class="py-5">
+            <h1>Enhed: {org.name}</h1>
+            <h1>Enhedstype: {org.unit_type.name}</h1>
+            <h1>Enhedsniveu: {org.org_unit_level.name}</h1>
+            <h1>
+              Gyldig fra: {new Date(org.validity.from).toUTCString()}
+            </h1>
+            <h1>
+              Gyldig til: {org.validity.to != "null"
+                ? "Ingen"
+                : new Date(org.validity.from).toUTCString()}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 {:catch error}
-	<div class="m-auto px-10">
-		<p>{error}</p>
-	</div>
+  <div class="m-auto px-10">
+    <p>{error}</p>
+  </div>
 {/await}
