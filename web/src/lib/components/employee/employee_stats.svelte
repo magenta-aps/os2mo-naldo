7ed0cf45e02cf2390a2465fa3b62201e1e4693d9
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-08-26 16:45:20 +0200

    feat: add employee stats and use the tab component

---
+++ web/src/lib/components/employee/employee_stats.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  // TODO: employee interface
+  export let employee: any
+
+  const calcEmploymentPeriod = (engagements: any[]): string => {
+    // Find and formats the first and last day of the employeement period
+    const firstDate = Math.min(...engagements.map((x) => Date.parse(x.validity.from)))
+    const lastDate = Math.max(...engagements.map((x) => Date.parse(x.validity.to)))
+
+    const formattedFirstDate = new Date(firstDate).toLocaleString("da-DK", {
+      dateStyle: "long",
+    })
+    const formattedLastDate = new Date(lastDate).toLocaleString("da-DK", {
+      dateStyle: "long",
+    })
+
+    return `${formattedFirstDate} - ${formattedLastDate}`
+  }
+</script>
+
+<div class="stats stats-vertical xl:stats-horizontal shadow bg-base-300 mb-6">
+  <div class="stat">
+    <div class="stat-title">MEDARBEJDER</div>
+    <div class="stat-value">{employee.name}</div>
+  </div>
+  <div class="stat">
+    <div class="stat-title">CPR-NUMMER</div>
+    <div class="stat-value">
+      {employee.cpr_no.slice(4)}-{employee.cpr_no.slice(-4)}
+    </div>
+  </div>
+  <div class="stat">
+    <div class="stat-title">ANSAT</div>
+    <div class="stat-value">{calcEmploymentPeriod(employee.engagements)}</div>
+  </div>
+</div>
--- web/src/routes/employee/[uuid].svelte
+++ web/src/routes/employee/[uuid].svelte
@@ -1,9 +1,73 @@
 <script lang="ts">
-	import { page } from '$app/stores'
+  import { page } from "$app/stores"
+  import { fetchGraph } from "$lib/util/http"
+  import Tabs from "$lib/components/shared/tabs.svelte"
+  import EmployeeStats from "$lib/components/employee/employee_stats.svelte"
+  import EmployeeTable from "$lib/components/employee/employee_table.svelte"
 
-	$: uuid = $page.params.uuid
+  const query = (uuid: string) => {
+    return `
+query {
+  employees(uuids: "${uuid}") {
+    objects {
+      name
+      engagements {
+        uuid
+        org_unit {
+          name
+        }
+        validity {
+          to
+          from
+        }
+      }
+      cpr_no
+    }
+  }
+}
+
+`
+  }
+  const fetchEmployee = async () => {
+    const res = await fetchGraph(query($page.params.uuid))
+    const json = await res.json()
+
+    console.log(json.data.employees[0].objects[0])
+
+    return json.data.employees[0].objects[0]
+  }
+
+  const calcEmploymentPeriod = (engagements: any[]): string => {
+    // Find and formats the first and last day of the employeement period
+    const firstDate = Math.min(...engagements.map((x) => Date.parse(x.validity.from)))
+    const lastDate = Math.max(...engagements.map((x) => Date.parse(x.validity.to)))
+
+    const formattedFirstDate = new Date(firstDate).toLocaleString("da-DK", {
+      dateStyle: "long",
+    })
+    const formattedLastDate = new Date(lastDate).toLocaleString("da-DK", {
+      dateStyle: "long",
+    })
+
+    return `${formattedFirstDate} - ${formattedLastDate}`
+  }
+
+  // For the tab categories
+  let items = ["Engagementer", "Adresser"]
+  let activeItem = items[0]
+  const tabChange = (e) => (activeItem = e.detail)
 </script>
 
-<div class="m-auto">
-	<h1 class="text-xl">{uuid}</h1>
+<div class="px-12 pt-6">
+  {#await fetchEmployee()}
+    loading...
+  {:then employee}
+    <EmployeeStats {employee} />
+    <Tabs {activeItem} {items} on:tabChange={tabChange} />
+    {#if activeItem === "Engagementer"}
+      Engagementer
+    {:else if activeItem === "Adresser"}
+      Adresser
+    {/if}
+  {/await}
 </div>
