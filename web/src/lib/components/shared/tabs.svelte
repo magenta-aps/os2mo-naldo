bc2caa8de594bf14d232b234f2811aace133041e
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-08-26 12:18:19 +0200

    feat: make dynamic tab component

---
+++ web/src/lib/components/shared/tabs.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte"
+  let dispatch = createEventDispatcher()
+
+  export let items: string[]
+  export let activeItem: string
+  export let tabFormat = "tab tab-lifted"
+</script>
+
+<div class="tabs">
+  {#each items as item}
+    <div
+      class="{tabFormat} {item === activeItem ? 'tab-active' : ''}"
+      on:click={() => dispatch("tabChange", item)}
+    >
+      {item}
+    </div>
+  {/each}
+</div>
