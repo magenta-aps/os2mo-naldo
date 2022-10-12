f160d17eec8601b12490dee8d438b3a50a306330
Andreas Brodersen <apb@magenta-aps.dk>
2022-10-12 11:35:32 +0200

    fix: refresh error on pages using page.ts load

---
+++ web/src/routes/+layout.ts
@@ -0,0 +1,3 @@
+// TODO: Should not be disabled when possible
+// Reason: https://stackoverflow.com/questions/71120958/fetch-error-when-refreshing-page-with-await-block-in-sveltekit
+export const ssr = false
