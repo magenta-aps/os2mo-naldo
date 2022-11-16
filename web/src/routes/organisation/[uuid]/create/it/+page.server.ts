0e5a0d01ac7f6eb8adcb0f7a5e4e8534dd7e6a8e
AndreasPB <andreas.p.brodersen@gmail.com>
2022-11-16 16:52:12 +0100

    feat: [#53626] add it create(wip) - org ituser create is not supported by our graphql

---
+++ web/src/routes/organisation/[uuid]/create/it/+page.server.ts
@@ -0,0 +1,35 @@
+import type { Actions, RequestEvent } from "@sveltejs/kit"
+
+export const actions: Actions = {
+  default: async ({ request, params }: RequestEvent): Promise<any> => {
+    const data = await request.formData()
+
+    // Yep...
+    const primary = data.get("primary")
+      ? "0644cd06-b84b-42e0-95fe-ce131c21fbe6"
+      : "afe0910b-df1f-478d-a62b-aa02492b3f67"
+
+    const query = `
+      mutation FrontendITUserUpdate($input: ITUserUpdateInput!) {
+        ituser_update(input: $input) {
+          uuid
+        }
+      }
+    `
+    return {
+      query,
+      variables: {
+        input: {
+          uuid: params.ituser,
+          primary,
+          validity: {
+            from: data.get("from"),
+            ...(data.get("to") && { to: data.get("to") }),
+          },
+          itsystem: data.get("itsystem"),
+          ...(data.get("user-key") && { user_key: data.get("user-key") }),
+        },
+      },
+    }
+  },
+}
