95c22be23bd9aded2e527f242d6fe0894fb7a637
Andreas Brodersen <apb@magenta-aps.dk>
2022-12-20 17:13:23 +0100

    temp

---
+++ web/src/routes/organisation/[uuid]/create/manager/+page.ts
@@ -0,0 +1,54 @@
+import { env } from "$env/dynamic/public"
+import { date } from "$lib/stores/date"
+import { keycloak } from "$lib/util/keycloak"
+import type { PageLoad } from "./$types"
+
+// TODO: Should not be disabled when possible
+// Reason: https://stackoverflow.com/questions/71120958/fetch-error-when-refreshing-page-with-await-block-in-sveltekit
+// Fixes being unable to refresh or open this route from a link
+export const ssr = false
+
+export const load: PageLoad = async (event) => {
+  let fromDate = ""
+  date.subscribe((v) => (fromDate = v))
+
+  const query = `
+      query {
+        facets(user_keys: ["org_unit_level", "org_unit_type"]) {
+          uuid
+          user_key
+          classes {
+            name
+            uuid
+          }
+        }
+        org_units(uuids: "${event.params.uuid}", from_date: "${fromDate}") {
+          objects {
+            uuid
+            name
+          }
+        }
+      }
+      `
+
+  const token = keycloak ? keycloak.token : "Keycloak disabled"
+  const res = await event.fetch(`${env.PUBLIC_BASE_URL}/graphql/v2`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify({
+      query: query,
+    }),
+  })
+  const json = await res.json()
+
+  if (json.data) {
+    return json.data
+  } else if (json.errors) {
+    throw new Error(json.errors[0].message)
+  } else {
+    throw new Error("Unknown error during data fetching")
+  }
+}
