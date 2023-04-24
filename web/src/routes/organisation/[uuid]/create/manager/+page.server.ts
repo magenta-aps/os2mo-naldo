abba246278044a6fb1cbe6688ed0c5886e56ccc5
Andreas Brodersen <apb@magenta-aps.dk>
2023-04-24 10:19:53 +0200

    temp

---
+++ web/src/routes/organisation/[uuid]/create/manager/+page.server.ts
@@ -0,0 +1,35 @@
+import type { Actions, RequestEvent } from "@sveltejs/kit"
+
+export const actions: Actions = {
+  default: async ({ request }: RequestEvent) => {
+    const data = await request.formData()
+
+    console.log(data.forEach((v, k) => console.log(k, v)))
+    const name = data.get("name")
+    const parent = data.get("select-org-tree")
+    const orgLevel = data.get("org-level")
+    const orgType = data.get("org-type")
+    const startDate = data.get("start-date")
+    const endDate = data.get("end-date")
+
+    const details: string[] = []
+    for (let i of data.entries()) {
+      if (i[0].startsWith("detail-")) {
+        details.push(JSON.parse(i[1].toString()))
+      }
+    }
+
+    return {
+      name: name,
+      parent: { uuid: parent },
+      org_unit_level: { uuid: orgLevel },
+      org_unit_type: { uuid: orgType },
+      validity: {
+        // End format YYYY-mm-dd
+        from: startDate,
+        to: endDate ? endDate : undefined,
+      },
+      details: details,
+    }
+  },
+}
