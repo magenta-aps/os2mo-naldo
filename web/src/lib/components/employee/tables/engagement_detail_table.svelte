a8324172be1d2329a3b1a7dfe458b0ded5b80263
Andreas Brodersen <apb@magenta-aps.dk>
2023-05-30 09:55:08 +0200

    refactor: [#56263] move employee and engagements into components + fix validity type error

---
+++ web/src/lib/components/employee/tables/engagement_detail_table.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import DetailTable from "$lib/components/shared/detail_table.svelte"
+  import { graphQLClient } from "$lib/util/http"
+  import { gql } from "graphql-request"
+  import { page } from "$app/stores"
+  import { base } from "$app/paths"
+  import { EngagementDetailDocument } from "./query.generated"
+  import ValidityTableCell from "$lib/components/shared/validity_table_cell.svelte"
+
+  export let uuid: string
+
+  gql`
+    query EngagementDetail($uuid: [UUID!]) {
+      employees(uuids: $uuid) {
+        objects {
+          engagements {
+            uuid
+            org_unit {
+              name
+              uuid
+            }
+            validity {
+              to
+              from
+            }
+            job_function {
+              name
+            }
+          }
+        }
+      }
+    }
+  `
+</script>
+
+<DetailTable headers={["Stillingsbetegnelse", "Enhed", "Dato"]}>
+  {#await graphQLClient().request(EngagementDetailDocument, { uuid: uuid })}
+    <tr class="p-4 leading-5 border-t border-slate-300 text-secondary">
+      <td class="p-4">Henter data...</td>
+    </tr>
+  {:then data}
+    {@const employee = data.employees[0].objects[0]}
+    {#each employee.engagements as engagement, i}
+      <tr
+        class="{i % 2 === 1 ? 'bg-slate-100' : ''} 
+            p-4 leading-5 border-t border-slate-300 text-secondary"
+      >
+        <td class="p-4">
+          {engagement.job_function.name}
+        </td>
+
+        <a href="{base}/organisation/{engagement.org_unit[0].uuid}">
+          <td class="p-4">
+            {engagement.org_unit[0].name}
+          </td>
+        </a>
+        <ValidityTableCell validity={engagement.validity} />
+      </tr>
+    {/each}
+  {/await}
+</DetailTable>
