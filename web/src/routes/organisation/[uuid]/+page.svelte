0bf1b999ed2dd907365a2f7f67080351771e48bd
Andreas Brodersen <apb@magenta-aps.dk>
2023-06-05 12:54:03 +0200

    refactor: [#xxxxx] move detail components out of load await + remove reduntant elements from query

--- web/src/routes/organisation/[uuid]/+page.svelte
+++ web/src/routes/organisation/[uuid]/+page.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
   import { page } from "$app/stores"
-  import ValidityTableCell from "$lib/components/shared/validity_table_cell.svelte"
   import Tabs from "$lib/components/shared/tabs.svelte"
-  import DetailTable from "$lib/components/shared/detail_table.svelte"
   import HeadTitle from "$lib/components/shared/head_title.svelte"
   import Breadcrumbs from "$lib/components/org/breadcrumbs.svelte"
   import CopyToClipboard from "$lib/components/copy_to_clipboard.svelte"
   import TenseTabs from "$lib/components/shared/tense_tabs.svelte"
   import { activeOrgTab } from "$lib/stores/tab"
-  import { base } from "$app/paths"
   import { load } from "./data"
   import { date } from "$lib/stores/date"
   import UnitDetailTable from "$lib/components/org/tables/unit_detail_table.svelte"
   import { tenses } from "$lib/stores/tenses"
-  import Icon from "$lib/components/icon.svelte"
   import AddressTable from "$lib/components/org/tables/address_table.svelte"
-  import OrgTable from "$lib/components/org/org_table.svelte"
   import EngagementTable from "$lib/components/org/tables/engagement_table.svelte"
   import AssociationTable from "$lib/components/org/tables/association_table.svelte"
   import ItSystemTable from "$lib/components/org/tables/it_system_table.svelte"
@@ -44,12 +39,10 @@
 
 <HeadTitle type="organization" />
 
-{#await load($page.params.uuid, $date)}
-  <div class="px-10">
+<div class="px-10">
+  {#await load($page.params.uuid, $date)}
     <p class="py-5">Loader organisation...</p>
-  </div>
-{:then orgUnits}
-  <div class="px-10">
+  {:then orgUnits}
     <Breadcrumbs currentOrg={orgUnits.present[0].name} uuid={$page.params.uuid} />
     <div class="flex gap-5">
       <h1 class="pb-4">{orgUnits.present[0].name}</h1>
@@ -87,123 +80,123 @@
           <p>Intet at vise</p>
         {/if}
       {/if}
-    {:else if activeItem === "Adresser"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <AddressTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <AddressTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <AddressTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-    {:else if activeItem === "Engagementer"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <EngagementTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <EngagementTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <EngagementTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-
-    {:else if activeItem === "Tilknytninger"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <AssociationTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <AssociationTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <AssociationTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-    {:else if activeItem === "IT"}
-        <TenseTabs />
-      <!-- TODO: Add 'Add'-button -->
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <ItSystemTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <ItSystemTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <ItSystemTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-    {:else if activeItem === "Roller"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <RoleTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <RoleTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <RoleTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-    {:else if activeItem === "Ledere"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <ManagerTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <ManagerTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <ManagerTable tense="past" uuid={$page.params.uuid} />
-      {/if}
-    {:else if activeItem === "KLE-opmærkninger"}
-      <TenseTabs />
-      TODO
-    {:else if activeItem === "Relateret"}
-      <TenseTabs />
-      <!-- TODO: future and past does not work. 
-      Waiting to see if this can be done through GraphQL -->
-      {#if $tenses.future}
-        <h2 class="mb-4">Fremtid</h2>
-        <RelatedTable tense="future" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.present}
-        <h2 class="mb-4">Nutid</h2>
-        <RelatedTable tense="present" uuid={$page.params.uuid} />
-      {/if}
-      {#if $tenses.past}
-        <h2 class="mb-4">Fortid</h2>
-        <RelatedTable tense="past" uuid={$page.params.uuid} />
-      {/if}
     {/if}
-  </div>
-{/await}
+  {/await}
+  {#if activeItem === "Adresser"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <AddressTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <AddressTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <AddressTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "Engagementer"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <EngagementTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <EngagementTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <EngagementTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "Tilknytninger"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <AssociationTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <AssociationTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <AssociationTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "IT"}
+    <TenseTabs />
+    <!-- TODO: Add 'Add'-button -->
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <ItSystemTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <ItSystemTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <ItSystemTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "Roller"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <RoleTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <RoleTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <RoleTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "Ledere"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <ManagerTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <ManagerTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <ManagerTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {:else if activeItem === "KLE-opmærkninger"}
+    <TenseTabs />
+    TODO
+  {:else if activeItem === "Relateret"}
+    <TenseTabs />
+    <!-- TODO: future and past does not work. 
+    Waiting to see if this can be done through GraphQL -->
+    {#if $tenses.future}
+      <h2 class="mb-4">Fremtid</h2>
+      <RelatedTable tense="future" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.present}
+      <h2 class="mb-4">Nutid</h2>
+      <RelatedTable tense="present" uuid={$page.params.uuid} />
+    {/if}
+    {#if $tenses.past}
+      <h2 class="mb-4">Fortid</h2>
+      <RelatedTable tense="past" uuid={$page.params.uuid} />
+    {/if}
+  {/if}
+</div>
