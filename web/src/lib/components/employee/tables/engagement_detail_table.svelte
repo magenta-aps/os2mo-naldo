d2eedc04cca05f0036f84e3b79832e29d08b3eeb
mb <mb@magenta-aps.dk>
2023-07-20 11:47:30 +0200

    feat: [#56431] edit/terminate engagement employee tab

--- web/src/lib/components/employee/tables/engagement_detail_table.svelte
+++ web/src/lib/components/employee/tables/engagement_detail_table.svelte
@@ -54,12 +54,12 @@
         </a>
         <ValidityTableCell validity={engagement.validity} />
         <td>
-          <a aria-disabled href="{base}/organisation/{$page.params.uuid}/edit/engagement/{engagement.uuid}">
+          <a aria-disabled href="{base}/employee/{$page.params.uuid}/edit/engagement/{engagement.uuid}">
             <Icon type="pen" />
           </a>
         </td>
         <td>
-          <a href="{base}/organisation/{$page.params.uuid}/terminate/engagement/{engagement.uuid}">
+          <a href="{base}/employee/{$page.params.uuid}/terminate/engagement/{engagement.uuid}">
             <Icon type="xmark" size="30" />
           </a></td
         >
---
+++ web/src/routes/employee/[uuid]/edit/engagement/[engagement]/+page.svelte
@@ -0,0 +1,212 @@
+<script lang="ts">
+  import DateInput from "$lib/components/forms/shared/date_input.svelte"
+  import Error from "$lib/components/alerts/error.svelte"
+  import Input from "$lib/components/forms/shared/input.svelte"
+  import Select from "$lib/components/forms/shared/select.svelte"
+  import { enhance } from "$app/forms"
+  import { goto } from "$app/navigation"
+  import { base } from "$app/paths"
+  import { success, error } from "$lib/stores/alert"
+  import { graphQLClient } from "$lib/util/http"
+  import type { SubmitFunction } from "./$types"
+  import {
+    EngagementAndFacetDocument,
+    UpdateEngagementDocument,
+  } from "./query.generated"
+  import { gql } from "graphql-request"
+  import { page } from "$app/stores"
+  import { date } from "$lib/stores/date"
+  import Checkbox from "$lib/components/forms/shared/checkbox.svelte"
+
+  let fromDate: string
+  let toDate: string
+  let orgUnitUuid: string
+  let user_key: string
+  let jobFunction: string
+  let engagementType: string
+
+  gql`
+    query EngagementAndFacet($uuid: [UUID!], $fromDate: DateTime) {
+      facets(
+        user_keys: ["engagement_type", "engagement_job_function", "primary_type"]
+      ) {
+        uuid
+        user_key
+        classes {
+          name
+          uuid
+        }
+      }
+      engagements(uuids: $uuid, from_date: $fromDate) {
+        objects {
+          uuid
+          user_key
+          employee {
+            uuid
+            name
+          }
+          engagement_type {
+            name
+          }
+          job_function {
+            name
+          }
+          primary {
+            uuid
+          }
+          validity {
+            from
+            to
+          }
+          org_unit {
+            uuid
+            name
+            validity {
+              from
+              to
+            }
+          }
+        }
+      }
+    }
+
+    mutation UpdateEngagement($input: EngagementUpdateInput!) {
+      engagement_update(input: $input) {
+        objects {
+          uuid
+          employee {
+            name
+          }
+        }
+      }
+    }
+  `
+
+  const handler: SubmitFunction =
+    () =>
+    async ({ result }) => {
+      if (result.type === "success" && result.data) {
+        try {
+          const mutation = await graphQLClient().request(UpdateEngagementDocument, {
+            input: result.data,
+          })
+          $success = {
+            message: `FIXME`,
+            uuid: $page.params.uuid,
+            type: "organisation",
+          }
+          setTimeout(() => goto(`${base}/organisation/${$page.params.uuid}/`), 200)
+        } catch (err) {
+          console.error(err)
+          $error = { message: err as string }
+        }
+      }
+    }
+</script>
+
+{#await graphQLClient().request( EngagementAndFacetDocument, { uuid: $page.params.engagement, fromDate: $date } )}
+  <!-- TODO: Should have a skeleton for the loading stage -->
+  Henter data...
+{:then data}
+  {@const engagement = data.engagements[0].objects[0]}
+  {@const facets = data.facets}
+  {@const primaryUuid = facets[1].classes[0].uuid}
+  {@const nonPrimaryUuid = facets[1].classes[2].uuid}
+  {@const minDate = engagement.org_unit[0].validity.from.split("T")[0]}
+  {@const maxDate = engagement.org_unit[0].validity.to?.split("T")[0]}
+
+  <title>Rediger {engagement?.employee[0].name} | OS2mo</title>
+
+  <div class="flex align-center px-6 pt-6 pb-4">
+    <h3 class="flex-1">Rediger {engagement.employee[0].name}</h3>
+  </div>
+
+  <div class="divider p-0 m-0 mb-4 w-full" />
+
+  <form method="post" class="mx-6" use:enhance={handler}>
+    <div class="w-1/2 min-w-fit bg-slate-100 rounded">
+      <div class="p-8">
+        <div class="flex flex-row gap-6">
+          <DateInput
+            bind:value={fromDate}
+            startValue={$date}
+            title="Startdato"
+            id="from"
+            min={minDate}
+            max={maxDate ? maxDate : null}
+          />
+          <DateInput
+            bind:value={toDate}
+            startValue={engagement.validity.to
+              ? engagement.validity.to.split("T")[0]
+              : null}
+            title="Slutdato"
+            id="to"
+            min={fromDate}
+            max={maxDate ? maxDate : null}
+          />
+        </div>
+        <div class="flex flex-row gap-6">
+          <Input
+            title="Organisationsenhed UUID"
+            id="org-unit-uuid"
+            bind:value={orgUnitUuid}
+            startValue={engagement.org_unit[0].uuid}
+            required={true}
+            extra_classes="basis-1/2"
+          />
+          <Input
+            title="ID"
+            id="user-key"
+            bind:value={user_key}
+            startValue={engagement.user_key}
+            extra_classes="basis-1/2"
+          />
+        </div>
+        <div class="flex flex-row gap-6">
+          <Select
+            title="Stillingsbetegnelse"
+            id="job-function"
+            bind:value={jobFunction}
+            iterable={facets[2].classes.sort((a, b) => (a.name > b.name ? 1 : -1))}
+            required={true}
+            extra_classes="basis-1/2"
+          />
+          <Select
+            title="Engagementstype"
+            id="engagement-type"
+            bind:value={engagementType}
+            iterable={facets[0].classes.sort((a, b) => (a.name > b.name ? 1 : -1))}
+            required={true}
+            extra_classes="basis-1/2"
+          />
+        </div>
+        <div class="flex">
+          <Checkbox
+            title="Primær"
+            id="primary"
+            startValue={engagement.primary?.uuid}
+            value={primaryUuid}
+            extra_classes="checkbox-primary"
+          />
+        </div>
+        <input hidden name="non-primary" id="non-primary" value={nonPrimaryUuid} />
+      </div>
+    </div>
+    <div class="flex py-6 gap-4">
+      <button
+        type="submit"
+        class="btn btn-sm btn-primary rounded normal-case font-normal text-base text-base-100"
+        >Rediger engagement</button
+      >
+      <button
+        type="button"
+        class="btn btn-sm btn-outline btn-primary rounded normal-case font-normal text-base"
+        on:click={() => goto(`${base}/employee/${$page.params.uuid}`)}
+      >
+        Annullér
+      </button>
+    </div>
+    <Error />
+  </form>
+{/await}
