fc244c60e589ec30bc96be3d838828d022424e83
mb <mb@magenta-aps.dk>
2023-08-09 09:29:18 +0200

    feat: [#56435] terminate address

---
+++ web/src/routes/employee/[uuid]/terminate/address/[address]/+page.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import DateInput from "$lib/components/forms/shared/date_input.svelte"
+  import Error from "$lib/components/alerts/error.svelte"
+  import { enhance } from "$app/forms"
+  import { base } from "$app/paths"
+  import { success, error } from "$lib/stores/alert"
+  import { graphQLClient } from "$lib/util/http"
+  import { AddressDocument, TerminateAddressDocument } from "./query.generated"
+  import { gql } from "graphql-request"
+  import { page } from "$app/stores"
+  import { date } from "$lib/stores/date"
+  import type { SubmitFunction } from "./$types"
+
+  let toDate: string
+
+  // TODO: When updating GraphQL remember to change `address_terminate` `at` to `input`, 
+  // which was changed in v8
+  gql`
+    query Address($uuid: [UUID!], $fromDate: DateTime!) {
+      addresses(uuids: $uuid, from_date: $fromDate) {
+        objects {
+          uuid
+          value
+          name
+          address_type {
+            name
+            scope
+          }
+          validity {
+            from
+            to
+          }
+        }
+      }
+    }
+
+    mutation TerminateAddress($input: AddressTerminateInput!) {
+      address_terminate(at: $input) {
+        uuid
+        objects {
+          validity {
+            to
+          }
+        }
+      }
+    }
+  `
+  const handler: SubmitFunction =
+    () =>
+    async ({ result }) => {
+      if (result.type === "success" && result.data) {
+        try {
+          const mutation = await graphQLClient().request(TerminateAddressDocument, {
+            input: result.data,
+          })
+
+          $success = {
+            message: `Adressen afsluttes d. ${mutation.address_terminate.objects[0].validity.to.split("T")[0]}`,
+            uuid: $page.params.uuid,
+            type: "employee",
+          }
+        } catch (err) {
+          console.error(err)
+          $error = { message: err as string }
+        }
+      }
+    }
+</script>
+
+{#await graphQLClient().request( AddressDocument, { uuid: $page.params.address, fromDate: $date} )}
+  <!-- TODO: Should have a skeleton for the loading stage -->
+  Henter data...
+{:then data}
+  {@const address = data.addresses[0].objects[0]}
+  {@const minDate = address.validity.from.split("T")[0]}
+  {@const addressValue = address.address_type.scope === "DAR" ? address.name : address.value}
+
+  <title>Afslut adresse | OS2mo</title>
+
+  <div class="flex align-center px-6 pt-6 pb-4">
+    <h3 class="flex-1">Afslut adressen - {address.address_type.name}: {addressValue}</h3>
+  </div>
+
+  <div class="divider p-0 m-0 mb-4 w-full" />
+
+  <form method="post" class="mx-6" use:enhance={handler}>
+    <div class="w-1/2 min-w-fit bg-slate-100 rounded">
+      <div class="p-8">
+        <DateInput
+          bind:value={toDate}
+          startValue={$date}
+          title="Slutdato"
+          id="to"
+          min={minDate}
+        />
+      </div>
+    </div>
+    <div class="flex py-6 gap-4">
+      <button
+        type="submit"
+        class="btn btn-sm btn-primary rounded normal-case font-normal text-base text-base-100"
+        >Afslut adresse</button
+      >
+      <a
+        href={`${base}/employee/${$page.params.uuid}`}
+        class="btn btn-sm btn-outline btn-primary rounded normal-case font-normal text-base"
+        >Annullér</a
+      >
+    </div>
+    <Error />
+  </form>
+{/await}
