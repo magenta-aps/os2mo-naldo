47faae1a5d548a0ff93ce68e9b9ee9a43767475d
mb <mb@magenta-aps.dk>
2023-08-11 13:36:40 +0200

    feat: [#56443] edit association

---
+++ web/src/routes/employee/[uuid]/edit/association/[association]/+page.server.ts
@@ -0,0 +1,24 @@
+import type { Actions, RequestEvent } from "@sveltejs/kit"
+import type { AssociationUpdateInput } from "$lib/graphql/types"
+
+export const actions: Actions = {
+  default: async ({
+    request,
+    params,
+  }: RequestEvent): Promise<AssociationUpdateInput> => {
+    const data = await request.formData()
+    const associationType = data.get("association-type")
+    const orgUnitUuid = data.get("org-unit-uuid")
+    const primary = data.get("primary")
+    const startDate = data.get("from")
+    const endDate = data.get("to")
+
+    return {
+      uuid: params.association,
+      validity: { from: startDate, ...(endDate && { to: endDate }) },
+      ...(associationType && { association_type: associationType }),
+      ...(orgUnitUuid && { org_unit: orgUnitUuid }),
+      ...(primary && { primary: primary }),
+    }
+  },
+}
