44e298edc1c0ee8f80bbb1236b1c153b7a096abb
Andreas Brodersen <apb@magenta-aps.dk>
2023-08-25 16:30:41 +0200

    feat: [#52096] employee search as you type PoC

---
+++ web/src/lib/components/employee_item.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let item: {
+    uuid: string
+    name: string
+    attrs: {
+      uuid: string
+      value: string
+      title: string
+    }[]
+  }
+
+  export let wantedAttrs: string[] | undefined = undefined
+</script>
+
+<div class="flex items-center cursor-default text-ellipsis">
+  <div class="text-ellipsis">
+    <div class="inline-block text-primary">{item.name}</div>
+    {#each item.attrs as attr}
+      {#if !wantedAttrs || wantedAttrs.includes(attr.title)}
+        <div class="inline-block text-secondary">{attr.value + " "}</div>
+      {/if}
+    {/each}
+  </div>
+</div>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
   import indexPageImage from "$lib/assets/index_page_image.png"
+  import NewSearch from "$lib/components/new_search.svelte"
 </script>
 
 <svelte:head>
   <title>OS2mo</title>
 </svelte:head>
 
+<NewSearch />
+
 <div class="m-auto">
   <img class="rounded-full" src={indexPageImage} alt="velkommen tilbage til os2mo" />
 </div>
+
