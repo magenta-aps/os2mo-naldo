8c4f6b54943044eb5c84b3854ba8be09d4f8349d
jrw <jrw@magenta.dk>
2023-08-31 12:51:29 +0200

    feat: [#56450] orlov rediger

--- web/src/routes/employee/[uuid]/terminate/engagement/[engagement]/+page.svelte
+++ web/src/routes/employee/[uuid]/terminate/engagement/[engagement]/+page.svelte
@@ -62,7 +62,7 @@
           $success = {
             message: `${mutation.engagement_terminate.objects[0].employee[0].name} afsluttes d. ${toDate}`,
             uuid: $page.params.uuid,
-            type: "employee",
+            type: "employee"
           }
         } catch (err) {
           console.error(err)
