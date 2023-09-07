52967e6e061f74fec729a90e142a62ac95dcc80a
Johanne Riis-Weitling <jrw@magenta-aps.dk>
2023-09-07 08:55:07 +0200

    feat: 56449 create leave

--- web/src/lib/util/helpers.ts
+++ web/src/lib/util/helpers.ts
@@ -10,3 +10,21 @@ export const getITUserITSystemName = (itusers: ITUserITSystemName[]) => {
     name: `${ituser.itsystem.name}, ${ituser.user_key}`,
   }))
 }
+
+// Used to display both job_function-name and org-name on a single line, for example, in a dropdown select.
+type EngagementTitleAndUuid = {
+  uuid: string
+  job_function: { name: string }
+  org_unit: { name: string }[]
+}
+
+export const getEngagementTitlesAndUuid = (
+  engagements: EngagementTitleAndUuid[]
+): { uuid: string; name: string }[] => {
+  return engagements.map((engagement) => {
+    return {
+      uuid: engagement.uuid,
+      name: `${engagement.job_function.name}, ${engagement.org_unit[0].name}`,
+    }
+  })
+}
