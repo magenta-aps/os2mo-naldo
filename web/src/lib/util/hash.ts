c93e310f45543a10a01e0f8f41b618f57495632d
jrw <jrw@magenta.dk>
2023-09-07 16:13:04 +0200

    fix: [#57427] ran prettier on hash.ts

--- web/src/lib/util/hash.ts
+++ web/src/lib/util/hash.ts
@@ -1,33 +1,33 @@
 export function toUrlFriendly(str: string): string {
-    return str
-        .toLowerCase()
-        .replace(/æ/g, "ae")
-        .replace(/ø/g, "oe")
-        .replace(/å/g, "aa")
-        .replace(/[^a-z0-9_\-]/g, "")  // accept "-"
-        .replace(/\s+/g, "_");  // replace space with underscore
+  return str
+    .toLowerCase()
+    .replace(/æ/g, "ae")
+    .replace(/ø/g, "oe")
+    .replace(/å/g, "aa")
+    .replace(/[^a-z0-9_\-]/g, "") // accept "-"
+    .replace(/\s+/g, "_") // replace space with underscore
 }
 
 export function fromUrlFriendly(urlString: string, tabNames: string[]): string {
-    const str = urlString.replace(/_/g, " ").replace("#", "");
-    for (let tab of tabNames) {
-        if (toUrlFriendly(tab).toLowerCase() === str.toLowerCase()) return tab;
-    }
-    return "";
+  const str = urlString.replace(/_/g, " ").replace("#", "")
+  for (let tab of tabNames) {
+    if (toUrlFriendly(tab).toLowerCase() === str.toLowerCase()) return tab
+  }
+  return ""
 }
 
 export function getHash(): string {
-    return window.location.hash.replace("#", "");
+  return window.location.hash.replace("#", "")
 }
 
 export function setHash(value: string): void {
-    window.location.hash = value;
+  window.location.hash = value
 }
 
 export function addUrlChangeListener(listener: () => void) {
-    window.addEventListener('popstate', listener);
+  window.addEventListener("popstate", listener)
 
-    return () => {
-        window.removeEventListener('popstate', listener);
-    };
+  return () => {
+    window.removeEventListener("popstate", listener)
+  }
 }
