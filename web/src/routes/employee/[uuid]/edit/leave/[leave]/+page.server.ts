4b11fe2c5720ef51a5c0660a863f776f706a4190
Johanne Riis-Weitling <jrw@magenta-aps.dk>
2023-09-13 10:03:50 +0200

    [#56450] Edit leave

---
+++ web/src/routes/employee/[uuid]/edit/leave/[leave]/+page.server.ts
@@ -0,0 +1,19 @@
+import type { Actions, RequestEvent } from "@sveltejs/kit"
+import type { LeaveUpdateInput } from "$lib/graphql/types"
+
+export const actions: Actions = {
+  default: async ({ request, params }: RequestEvent): Promise<LeaveUpdateInput> => {
+    const data = await request.formData()
+    const leaveTypeUuid = data.get("leave-type-uuid")
+    const engagementUuid = data.get("engagement-uuid")
+    const startDate = data.get("from")
+    const endDate = data.get("to")
+
+    return {
+      uuid: params.leave,
+      ...(leaveTypeUuid && { leave_type: leaveTypeUuid }),
+      ...(engagementUuid && { engagement: engagementUuid }),
+      validity: { from: startDate, ...(endDate && { to: endDate }) },
+    }
+  },
+}
