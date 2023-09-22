279a1d2f5ea8f89d90627034e628d374b0948efa
mb <mb@magenta-aps.dk>
2023-09-22 15:18:51 +0200

    feat: [#57718] terminate employee

---
+++ web/src/routes/employee/terminate/+page.server.ts
@@ -0,0 +1,15 @@
+import type { Actions, RequestEvent } from "@sveltejs/kit"
+import type { EmployeeTerminateInput } from "$lib/graphql/types"
+
+export const actions: Actions = {
+  default: async ({ request }: RequestEvent): Promise<EmployeeTerminateInput> => {
+    const data = await request.formData()
+    const toDate = data.get("to")
+    const employee = data.get("employee-uuid")
+
+    return {
+      uuid: employee,
+      to: toDate,
+    }
+  },
+}
