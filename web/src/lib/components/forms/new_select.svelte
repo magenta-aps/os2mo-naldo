fef16c9f543e8918f548e627b300c5b0d46853d0
mb <mb@magenta-aps.dk>
2023-09-27 12:17:08 +0200

    feat: [#57235] support multi selects

---
+++ web/src/lib/components/forms/new_select.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import SvelteSelect from "svelte-select"
+  export let title: string | undefined = undefined
+  export let id: string
+  export let iterable: any[] | undefined = undefined
+  export let required = false
+  export let placeholder: string = ""
+  export let disabled = false
+  export let multiple = false
+  // export let startValue: string | undefined = undefined
+
+  const itemId = "uuid" // Used by the component to differentiate between items
+
+  const floatingConfig = {
+    placement: "bottom-start",
+    strategy: "fixed",
+  }
+</script>
+
+<div class="w-full pb-4 cursor-pointer">
+  {#if title}
+    <label for={id} class="text-sm text-secondary pb-1">
+      {title}
+    </label>
+  {/if}
+  <SvelteSelect
+    --font-size="1rem"
+    --height="2rem"
+    --loading-height="1.5rem"
+    --loading-width="1.5rem"
+    --spinner-height="1.5rem"
+    --spinner-width="1.5rem"
+    --item-padding="0.25rem 0.75rem 0.25rem 0.75rem"
+    --item-height="auto"
+    --item-line-height="auto"
+    --border-radius="0.25rem"
+    --placeholder-color="#00244E"
+    --icons-color="#00244E"
+    --border-focused="solid 0px"
+    --padding="0.75rem 0.75rem"
+    {id}
+    name={id}
+    {floatingConfig}
+    {required}
+    {disabled}
+    {itemId}
+    showChevron={true}
+    clearable={false}
+    {placeholder}
+    items={iterable}
+    {multiple}
+    searchable={false}
+    on:change
+    on:clear
+  >
+    <div slot="item" let:item class="cursor-pointer">
+      {item.name}
+    </div>
+
+    <div slot="selection" let:selection class="cursor-pointer">
+      {selection.name}
+    </div>
+  </SvelteSelect>
+</div>
+
+<style>
+  :global(.svelte-select.focused) {
+    box-shadow: 0px 0px 0px 3px #1053ab;
+  }
+</style>
--- web/src/routes/organisation/[uuid]/create/manager/+page.server.ts
+++ web/src/routes/organisation/[uuid]/create/manager/+page.server.ts
@@ -1,14 +1,25 @@
 import type { Actions, RequestEvent } from "@sveltejs/kit"
 import type { ManagerCreateInput } from "$lib/graphql/types"
 
+type UnpackedClass = {
+  name: string
+  uuid: string
+  user_key: string
+}[]
+
 export const actions: Actions = {
   default: async ({ request, params }: RequestEvent): Promise<ManagerCreateInput> => {
     const data = await request.formData()
     const employeeUuid = data.get("employee-uuid")
     const managerType = data.get("manager-type")
     const managerLevel = data.get("manager-level")
-    // Needs support for adding more than 1 responsibility
-    const responsibility = data.get("responsibility")
+    // Keeping these lines as documentation for the following one-liner.
+    // const jsonResponsibility = data.get("responsibility") as string
+    // const responsibility = JSON.parse(jsonResponsibility) as Class
+    // const resp = responsibility.map(v => v.uuid)
+    const resp = (
+      JSON.parse(data.get("responsibility") as string) as UnpackedClass
+    ).map((v) => v.uuid)
     const startDate = data.get("from")
     const endDate = data.get("to")
 
@@ -16,7 +27,7 @@ export const actions: Actions = {
       org_unit: params.uuid,
       manager_type: managerType,
       manager_level: managerLevel,
-      responsibility: [responsibility],
+      responsibility: resp,
       validity: { from: startDate, ...(endDate && { to: endDate }) },
       ...(employeeUuid && { person: employeeUuid }),
     }
