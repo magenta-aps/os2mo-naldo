dcdcb70fe20685bb0ef2f8b5d783cbb1ff669a76
mb <mb@magenta-aps.dk>
2023-12-05 14:23:52 +0100

    chore: [#xxxxx] remove placeholder color

--- web/src/lib/components/DARSearch.svelte
+++ web/src/lib/components/DARSearch.svelte
@@ -61,7 +61,6 @@
       --clear-select-width="1.5rem"
       --value-container-padding="0rem"
       --border-radius="0.25rem"
-      --placeholder-color="#00244E"
       --icons-color="#00244E"
       --padding="0 0.75rem 0 0.75rem"
       placeholder="Søg efter adresse"
