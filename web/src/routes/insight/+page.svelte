375272a9d3d745af8f1064eca9e536efbd58e4f7
mb <mb@magenta-aps.dk>
2024-01-29 10:16:24 +0100

    feat: [#59250] search button

--- web/src/routes/insight/+page.svelte
+++ web/src/routes/insight/+page.svelte
@@ -1,22 +1,52 @@
 <script lang="ts">
   import { gql } from "graphql-request"
   import { graphQLClient } from "$lib/util/http"
+  import { date } from "$lib/stores/date"
   import HeadTitle from "$lib/components/shared/head_title.svelte"
   import DetailTable from "$lib/components/shared/detail_table.svelte"
   import InsightTable from "$lib/components/tables/InsightTable.svelte"
   import Input from "$lib/components/forms/shared/input.svelte"
   import Select from "$lib/components/forms/shared/select.svelte"
-  import { GetClassesDocument } from "./query.generated"
+  import {
+    GetClassesDocument,
+    GetEngagementsDocument,
+    type GetEngagementsQuery,
+  } from "./query.generated"
   import { getClassesByFacetUserKey } from "$lib/util/get_classes"
   import Search from "$lib/components/search.svelte"
   import { engagements } from "$lib/stores/csv"
   import { downloadHandler } from "$lib/util/csv"
+  import { onMount } from "svelte"
+  import { debounce } from "$lib/util/helpers"
+
+  type Engagements = GetEngagementsQuery["engagements"]["objects"][0]["current"][]
 
   let name: string,
     jobFunction: { uuid: string; name: string; user_key?: string | null },
     jobFunctionUuid: string,
     orgUnit: { uuid: string; name: string; user_key?: string | null },
     orgUnitUuid: string
+
+  const filterEngagements = async (
+    name: string | undefined | null,
+    jobFunctionUuid: string | undefined | null,
+    orgUnitUuid: string | undefined | null
+  ): Promise<Engagements> => {
+    const res = await graphQLClient().request(GetEngagementsDocument, {
+      queryString: name,
+      jobFunction: jobFunctionUuid,
+      orgUnit: orgUnitUuid,
+      fromDate: $date,
+    })
+    const filteredEngagements: Engagements = []
+
+    // Filters and flattens the data
+    for (const outer of res.engagements.objects) {
+      filteredEngagements.push(outer.current)
+    }
+
+    return ($engagements = filteredEngagements)
+  }
 
   $: {
     jobFunctionUuid = jobFunction?.uuid
@@ -39,6 +69,50 @@
         }
       }
     }
+
+    query GetEngagements(
+      $limit: int = 10
+      $queryString: String
+      $orgUnit: [UUID!]
+      $jobFunction: [UUID!]
+      $fromDate: DateTime
+    ) {
+      engagements(
+        limit: $limit
+        filter: {
+          employee: { query: $queryString }
+          org_unit: { uuids: $orgUnit }
+          job_function: { uuids: $jobFunction }
+          from_date: $fromDate
+        }
+      ) {
+        objects {
+          current {
+            person {
+              name
+            }
+            primary {
+              name
+            }
+            job_function {
+              name
+            }
+            org_unit {
+              name
+              managers {
+                person {
+                  name
+                }
+              }
+            }
+            validity {
+              from
+              to
+            }
+          }
+        }
+      }
+    }
   `
 
   const allPossibleHeaders = [
@@ -50,12 +124,16 @@
     { key: "validity.from", label: "Validity From" },
     { key: "validity.to", label: "Validity To" },
   ]
+
+  onMount(async () => {
+    $engagements = await filterEngagements(name, jobFunctionUuid, orgUnitUuid)
+  })
 </script>
 
 <HeadTitle type="insight" />
 
-<div class="px-12 pt-6">
-  <h1 class="mb-4">Insights</h1>
+<div class="px-12 pt-6 mb-5">
+  <h1 class="mb-4">Insight</h1>
   {#await graphQLClient().request(GetClassesDocument)}
     <!-- TODO: Should have a skeleton for the loading stage -->
     Henter data...
@@ -63,7 +141,9 @@
     {@const facets = data.facets.objects}
     <div class="collapse bg-slate-100 rounded shadow-sm my-4">
       <input type="checkbox" />
-      <div class="collapse-title">Filter</div>
+      <div class="collapse-title">
+        <h3>Filter</h3>
+      </div>
       <div class="collapse-content">
         <div class="grid grid-cols-4 gap-4">
           <Input title="Navn" id="filter-name" bind:value={name} />
@@ -82,6 +162,12 @@
             bind:value={orgUnit}
           />
         </div>
+        <button
+          class="btn btn-sm btn-primary rounded normal-case font-normal text-base text-base-100"
+          on:click={async () =>
+            await debounce(filterEngagements, name, jobFunctionUuid, orgUnitUuid)}
+          >Søg</button
+        >
       </div>
     </div>
 
@@ -96,12 +182,11 @@
     >
       <svelte:component this={InsightTable} {name} {jobFunctionUuid} {orgUnitUuid} />
     </DetailTable>
+    <button
+      class="btn btn-sm btn-primary rounded normal-case font-normal text-base text-base-100"
+      disabled={!$engagements.length}
+      on:click={(event) => downloadHandler(event, $engagements, allPossibleHeaders)}
+      >Download CSV</button
+    >
   {/await}
-
-  <button
-    class="btn btn-sm btn-primary rounded normal-case font-normal text-base text-base-100"
-    disabled={!$engagements.length}
-    on:click={(event) => downloadHandler(event, $engagements, allPossibleHeaders)}
-    >Download CSV</button
-  >
 </div>
