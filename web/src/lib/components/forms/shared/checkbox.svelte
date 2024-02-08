89b23a80e0815c0f13b8a7a0119738b4aa30759b
mb <mb@magenta-aps.dk>
2024-02-08 11:26:56 +0100

    feat: [#58777] roles on ituser create + minor tweaks

--- web/src/lib/components/forms/shared/checkbox.svelte
+++ web/src/lib/components/forms/shared/checkbox.svelte
@@ -4,11 +4,16 @@
   export let value: string
   export let startValue: string | undefined = undefined
   export let extra_classes = ""
+  export let noPadding: boolean = false
   export let disabled: boolean = false
 </script>
 
-<div class="form-control">
-  <label class="label cursor-pointer p-0 gap-4 {disabled ? 'text-gray-400' : ''}">
+<div class="form-control {noPadding ? '' : 'pb-3'}">
+  <label
+    class="label cursor-pointer p-0 {noPadding ? '' : 'pb-1'} gap-4 {disabled
+      ? 'text-gray-400'
+      : ''}"
+  >
     <input
       type="checkbox"
       {id}
