a33ab4cf364c251471150aa2cfe8e8cab55181be
mb <mb@magenta-aps.dk>
2024-02-13 14:20:30 +0100

    feat: [#59709] make MO confdb store

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -6,9 +6,37 @@
   import { initKeycloak } from "$lib/util/keycloak"
   import Favicon from "$lib/assets/favicon.png"
   import Drawer from "$lib/components/drawer.svelte"
+  import { MOConfig, formatConfig } from "$lib/stores/config"
+  import { gql } from "graphql-request"
+  import { GetConfigDocument } from "./query.generated"
+  import { graphQLClient } from "$lib/util/http"
+
+  gql`
+    query GetConfig {
+      configuration {
+        objects {
+          key
+          jsonified_value
+        }
+      }
+    }
+  `
+
+  async function getConfig() {
+    try {
+      const config = await graphQLClient().request(GetConfigDocument)
+      return formatConfig(config)
+    } catch (error) {
+      console.error("Error fetching configuration:", error)
+      return {}
+    }
+  }
 
   onMount(async () => {
     await initKeycloak()
+    // Will run into race conditions if store is not
+    // retrieved inside an onMount, when used elsewhere
+    $MOConfig = await getConfig()
   })
 </script>
 
--- web/src/routes/query.generated.ts
+++ web/src/routes/query.generated.ts
@@ -1,10 +1,16 @@
 import type * as Types from '../lib/graphql/types';
 
 import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type GetConfigQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type GetConfigQuery = { __typename?: 'Query', configuration: { __typename?: 'ConfigurationPaged', objects: Array<{ __typename?: 'Configuration', key: string, jsonified_value: string }> } };
+
 export type VersionQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
 export type VersionQuery = { __typename?: 'Query', version: { __typename?: 'Version', mo_version?: string | null } };
 
 
+export const GetConfigDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetConfig"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"configuration"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"objects"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"key"}},{"kind":"Field","name":{"kind":"Name","value":"jsonified_value"}}]}}]}}]}}]} as unknown as DocumentNode<GetConfigQuery, GetConfigQueryVariables>;
 export const VersionDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Version"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"version"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"mo_version"}}]}}]}}]} as unknown as DocumentNode<VersionQuery, VersionQueryVariables>;
