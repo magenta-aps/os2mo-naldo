abd96c3eab5c34395f9fe0eea4e5ab71466b2338
mb <mb@magenta-aps.dk>
2024-04-11 12:04:29 +0200

    feat: [#60452] add engagement user_key

--- web/src/lib/util/translationUtils.ts
+++ web/src/lib/util/translationUtils.ts
@@ -1,3 +1,7 @@
 export const capital = (str: string) => {
   return str.replace(/(^|\s)\S/, (l) => l.toLocaleUpperCase())
 }
+
+export const upperCase = (str: string) => {
+  return str.toUpperCase()
+}
