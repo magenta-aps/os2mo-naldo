048e1f7f5bd010273dad4a251be733f3db1b2790
mb <mb@magenta-aps.dk>
2024-09-20 08:39:21 +0200

    feat: [#62225] close dropdown on click

--- web/src/lib/components/navbar/NavLinks.svelte
+++ web/src/lib/components/navbar/NavLinks.svelte
@@ -5,34 +5,46 @@
   import { env } from "$env/dynamic/public"
   import { isAdmin } from "$lib/stores/auth"
   import { MOConfig } from "$lib/stores/config"
+
+  let openDropdown: boolean = false
+
+  const handleClickItem = () => {
+    openDropdown = false
+  }
 </script>
 
 <ul class="menu menu-horizontal px-1 hidden xl:flex">
   <li>
-    <details>
+    <details bind:open={openDropdown}>
       <summary class="flex font-bold"
         >{capital($_("link", { values: { n: 2 } }))}</summary
       >
       <ul class="dropdown-content p-2 menu z-[1] rounded w-max">
         <li>
-          <a class="w-100 text-secondary hover:no-underline" href="{base}/reports"
-            >{capital($_("report", { values: { n: 2 } }))}</a
+          <a
+            on:click={handleClickItem}
+            class="w-100 text-secondary hover:no-underline"
+            href="{base}/reports">{capital($_("report", { values: { n: 2 } }))}</a
           >
         </li>
         {#if $isAdmin}
           <!-- TODO: Create real featureflag that locks the page, and doesn't just remove the link -->
           {#if env.PUBLIC_SHOW_ADMIN_PANEL !== "false"}
             <li>
-              <a class="w-100 text-secondary hover:no-underline" href="{base}/admin"
-                >{capital($_("classifications"))}</a
+              <a
+                on:click={handleClickItem}
+                class="w-100 text-secondary hover:no-underline"
+                href="{base}/admin">{capital($_("classifications"))}</a
               >
             </li>
           {/if}
           <!-- TODO: Create real featureflag that locks the page, and doesn't just remove the link -->
           {#if env.PUBLIC_SHOW_INSIGHTS !== "false"}
             <li>
-              <a class="w-100 text-secondary hover:no-underline" href="{base}/insights"
-                >{capital($_("insights"))}</a
+              <a
+                on:click={handleClickItem}
+                class="w-100 text-secondary hover:no-underline"
+                href="{base}/insights">{capital($_("insights"))}</a
               >
             </li>
           {/if}
@@ -41,8 +53,10 @@
           {@const links = JSON.parse($MOConfig.navlinks)}
           {#each links as link}
             <li>
-              <a class="w-100 text-secondary hover:no-underline" href={link.href}
-                >{link.text}</a
+              <a
+                on:click={handleClickItem}
+                class="w-100 text-secondary hover:no-underline"
+                href={link.href}>{link.text}</a
               >
             </li>
           {/each}
