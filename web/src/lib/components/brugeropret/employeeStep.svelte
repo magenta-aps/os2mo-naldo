7b29c3b1c23262f572336faa766641f71322d984
mb <mb@magenta-aps.dk>
2024-10-02 14:57:18 +0200

    feat: [#61198] support CPRLookup - TODO: fix 'normal' cpr

--- web/src/lib/components/brugeropret/employeeStep.svelte
+++ web/src/lib/components/brugeropret/employeeStep.svelte
@@ -5,6 +5,9 @@
   import { step } from "$lib/stores/stepStore"
   import Error from "$lib/components/alerts/Error.svelte"
   import Input from "$lib/components/forms/shared/Input.svelte"
+  import { form, field } from "svelte-forms"
+  import { required, pattern, min } from "svelte-forms/validators"
+  import CprLookup from "$lib/components/CPRLookup.svelte"
   import { graphQLClient } from "$lib/util/http"
   import { GetSpConfigDocument } from "./query.generated"
   import { gql } from "graphql-request"
@@ -21,10 +24,36 @@
     }
   `
 
+  $: {
+    $employeeInfo.firstName = $employeeInfo.cprNumber?.name
+      ?.split(" ")
+      .slice(0, -1)
+      .join(" ")
+    $employeeInfo.lastName = $employeeInfo.cprNumber?.name
+      ?.split(" ")
+      .slice(-1)
+      .join(" ")
+  }
+
+  const cprNumber = field("cpr_number", "", [required(), pattern(/^\d{6}\d{4}$/)])
+  const firstName = field("first_name", "", [required()])
+  const lastName = field("last_name", "", [required()])
+  const svelteForm = form(cprNumber, firstName, lastName)
+
+  const validateForm = async () => {
+    await svelteForm.validate()
+    if ($svelteForm.valid) {
+      employeeInfo.isValid(true)
+      step.updateStep("inc")
+    } else {
+      employeeInfo.isValid(false)
+    }
+  }
+
   // TODO: Add error-handling
 </script>
 
-<form on:submit|preventDefault={() => step.updateStep("inc")}>
+<form on:submit|preventDefault={async () => await validateForm()}>
   {#await graphQLClient().request(GetSpConfigDocument)}
     <div class="sm:w-full md:w-3/4 xl:w-1/2 bg-slate-100 rounded">
       <div class="p-8">
@@ -45,28 +74,92 @@
     <div class="sm:w-full md:w-3/4 xl:w-1/2 bg-slate-100 rounded">
       <div class="p-8">
         <!-- TODO: Support SP, like in `employee -> create` -->
-        <Input
-          title={capital($_("cpr_number"))}
-          id="cpr-number"
-          bind:value={$employeeInfo.cprNumber}
-          required={true}
-        />
-        <div class="flex flex-row gap-6">
+        {#if SpEnabled}
+          <CprLookup
+            title={capital($_("cpr_number"))}
+            id="cpr-number"
+            bind:value={$employeeInfo.cprNumber}
+            bind:cprNumber={$cprNumber.value}
+            errors={$cprNumber.errors}
+          />
+          <!-- If we get a response with empty name property, it means that a fictional CPR-number has been entered. -->
+          <!-- Therefore we allow typing a name, rather than making the input read-only. -->
+          {#if $employeeInfo.cprNumber && $employeeInfo.cprNumber.name === ""}
+            <div class="flex flex-row gap-6">
+              <Input
+                title={capital($_("givenname", { values: { n: 2 } }))}
+                id="first-name"
+                bind:value={$employeeInfo.firstName}
+                bind:cprName={$firstName.value}
+                errors={$firstName.errors}
+                extra_classes="basis-1/2"
+                required={true}
+              />
+              <Input
+                title={capital($_("surname"))}
+                id="last-name"
+                bind:value={$employeeInfo.lastName}
+                bind:cprName={$lastName.value}
+                errors={$lastName.errors}
+                extra_classes="basis-1/2"
+                required={true}
+              />
+            </div>
+          {:else}
+            <div class="flex flex-row gap-6">
+              <Input
+                title={capital($_("givenname", { values: { n: 2 } }))}
+                id="first-name"
+                bind:value={$employeeInfo.firstName}
+                bind:cprName={$firstName.value}
+                errors={$firstName.errors}
+                extra_classes="basis-1/2"
+                required={true}
+                readonly
+              />
+              <Input
+                title={capital($_("surname"))}
+                id="last-name"
+                bind:value={$employeeInfo.lastName}
+                bind:cprName={$lastName.value}
+                errors={$lastName.errors}
+                extra_classes="basis-1/2"
+                required={true}
+                readonly
+              />
+            </div>
+          {/if}
+        {:else}
+          <!-- FIXME: Doesn't work correctly -->
+          <!-- Hard to support both SP and no SP in a smart way, when we need to store the data -->
           <Input
-            title={capital($_("givenname", { values: { n: 2 } }))}
-            id="first-name"
-            bind:value={$employeeInfo.firstName}
-            extra_classes="basis-1/2"
+            title={capital($_("cpr_number"))}
+            id="cpr-number"
+            bind:cprName={$cprNumber.value}
+            errors={$cprNumber.errors}
             required={true}
           />
-          <Input
-            title={capital($_("surname"))}
-            id="last-name"
-            bind:value={$employeeInfo.lastName}
-            extra_classes="basis-1/2"
-            required={true}
-          />
-        </div>
+          <div class="flex flex-row gap-6">
+            <Input
+              title={capital($_("givenname", { values: { n: 2 } }))}
+              id="first-name"
+              bind:value={$employeeInfo.firstName}
+              bind:cprName={$firstName.value}
+              errors={$firstName.errors}
+              extra_classes="basis-1/2"
+              required={true}
+            />
+            <Input
+              title={capital($_("surname"))}
+              id="last-name"
+              bind:value={$employeeInfo.lastName}
+              bind:cprName={$lastName.value}
+              errors={$lastName.errors}
+              extra_classes="basis-1/2"
+              required={true}
+            />
+          </div>
+        {/if}
         <div class="flex flex-row gap-6">
           <Input
             title={capital($_("nickname_givenname", { values: { n: 2 } }))}
