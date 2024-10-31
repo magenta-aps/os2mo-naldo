3885f2585b168a81729e2e2947739329ec5b2d95
mb <mb@magenta-aps.dk>
2024-10-31 15:47:34 +0100

    feat: [#xxxxx] redirect away from admin/insights if ff not set

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -2,8 +2,18 @@ import { browser } from "$app/environment"
 import "$lib/i18n" // Import to initialize. Important :)
 import { locale, waitLocale } from "svelte-i18n"
 import type { LayoutLoad } from "./$types"
+import { redirect } from "@sveltejs/kit"
+import { env } from "$env/dynamic/public"
 
-export const load: LayoutLoad = async () => {
+export const load: LayoutLoad = async ({ url }) => {
+  // Redirect if not true
+  if (env.PUBLIC_SHOW_INSIGHTS !== "true" && url.pathname.includes("insights")) {
+    throw redirect(302, "/")
+  }
+  // Redirect if not true
+  if (env.PUBLIC_SHOW_ADMIN_PANEL !== "true" && url.pathname.includes("admin")) {
+    throw redirect(302, "/")
+  }
   if (browser) {
     locale.set(window.navigator.language)
   }
