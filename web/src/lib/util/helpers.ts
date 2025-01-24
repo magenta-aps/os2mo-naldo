e6a610f10d266ee072f07497daf17a87a90ccf2f
mb <mb@magenta-aps.dk>
2025-01-24 13:16:03 +0100

    feat: [#63604] add retry

--- web/src/lib/util/helpers.ts
+++ web/src/lib/util/helpers.ts
@@ -73,11 +73,13 @@ export const paginateQuery = async (
   variableValues: Record<string, any> = {},
   pageSize: number,
   onProgress: (count: number) => void = () => {},
-  abortSignal?: AbortSignal
-) => {
+  abortSignal?: AbortSignal,
+  maxRetries: number = 30
+): Promise<any[]> => {
   let nextCursor = null
-  const results = []
+  const results: any[] = []
   let requestCount = 0
+
   try {
     while (true) {
       if (abortSignal?.aborted) {
@@ -86,26 +88,55 @@ export const paginateQuery = async (
 
       requestCount++
       onProgress(requestCount)
+
       const variables = {
         ...variableValues,
         limit: pageSize,
         cursor: nextCursor,
       }
 
-      const result: any = await graphQLClient().request(query, variables)
+      for (let attempts = 0; attempts <= maxRetries; attempts++) {
+        try {
+          const result: any = await graphQLClient().request(query, variables)
 
-      for (const obj of result.page.objects) {
-        results.push(obj)
+          // Add results to the array
+          for (const obj of result.page.objects) {
+            results.push(obj)
+          }
+          nextCursor = result.page["page_info"]["next_cursor"]
+          break
+        } catch (error: any) {
+          console.error("Error encountered during request:", error)
+          const errorMessage = error?.response?.errors?.[0]?.message || error.message
+
+          if (
+            errorMessage &&
+            errorMessage.toLowerCase().includes("signature has expired")
+          ) {
+            console.warn(
+              `Retrying request (${
+                attempts + 1
+              }/${maxRetries}) due to signature expiration.`
+            )
+            // 1-second delay on retry
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+          } else {
+            console.error("Failed after retries or non-retryable error:", error)
+            throw error
+          }
+        }
       }
-      nextCursor = result.page["page_info"]["next_cursor"]
+
       if (!nextCursor) break // Exit if no more pages
     }
   } catch (error) {
+    console.error("Error during pagination:", error)
     throw error
   }
 
   return results
 }
+
 export const getValidities = async (uuid: string) => {
   const res = await graphQLClient().request(GetOrgUnitValiditiesDocument, {
     uuid: uuid,
