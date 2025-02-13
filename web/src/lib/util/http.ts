3e2f4d5e11f0b99fa3447da8f497da8d6f490603
Miklas Bøgvald <mb@magenta-aps.dk>
2025-02-13 10:34:50 +0100

    feat: [#63867] bump graphql v23 + support terminated kle_numbers

--- web/src/lib/util/http.ts
+++ web/src/lib/util/http.ts
@@ -4,7 +4,7 @@ import { GraphQLClient } from "graphql-request"
 
 // Is exported as a function to delay evaluation of till the client is ready
 export const graphQLClient = (signal?: AbortSignal) => {
-  return new GraphQLClient(`${env.PUBLIC_BASE_URL}/graphql/v22`, {
+  return new GraphQLClient(`${env.PUBLIC_BASE_URL}/graphql/v23`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + keycloak?.token,
