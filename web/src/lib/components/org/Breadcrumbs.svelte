20408a1f80f7afa2eff1baa24b9a01c40b03562e
mb <mb@magenta-aps.dk>
2025-02-13 10:49:48 +0100

    refactor: [#xxxxx] refactor breadcrumbs

--- web/src/lib/components/org/Breadcrumbs.svelte
+++ web/src/lib/components/org/Breadcrumbs.svelte
@@ -3,34 +3,55 @@
   import { capital } from "$lib/util/translationUtils"
   import { base } from "$app/paths"
   import { date } from "$lib/stores/date"
-  import { fetchParentTree } from "$lib/util/parent_tree"
+  import { graphQLClient } from "$lib/util/http"
+  import { gql } from "graphql-request"
+  import { GetBreadcrumbsDocument } from "./query.generated"
 
   export let orgUnit: { name: string; uuid: string } | null | undefined = undefined
   export let emptyMessage: string = ""
   export let link: boolean = false
+  gql`
+    query GetBreadcrumbs($uuid: [UUID!], $currentDate: DateTime) {
+      org_units(filter: { uuids: $uuid }) {
+        objects {
+          current(at: $currentDate) {
+            ancestors {
+              name
+              uuid
+            }
+          }
+        }
+      }
+    }
+  `
 </script>
 
 <!-- Since link is only used at Organisation pages, which is where we need no padding -->
 <!-- I used the conditional for padding - even though it's not directly connected. -->
 {#if orgUnit}
-  {#await fetchParentTree(orgUnit.uuid, $date)}
+  {#await graphQLClient().request( GetBreadcrumbsDocument, { uuid: orgUnit.uuid, currentDate: $date } )}
     <div class="text-secondary {link ? 'pb-0' : 'pb-4'}">
       {capital($_("loading"))}...
     </div>
-  {:then parents}
+  {:then data}
+    {@const parents = data.org_units.objects.length
+      ? data.org_units.objects[0].current?.ancestors
+      : []}
     <div class="breadcrumbs text-secondary pt-0 {link ? 'pb-0' : 'pb-4'}">
       <ul>
-        {#each parents.reverse() as parent}
-          <li>
-            {#if link}
-              <a href="{base}/organisation/{parent.uuid}">
-                {parent.name}
-              </a>
-            {:else}
-              <p>{parent.name}</p>
-            {/if}
-          </li>
-        {/each}
+        {#if parents}
+          {#each parents.reverse() as parent}
+            <li>
+              {#if link}
+                <a href="{base}/organisation/{parent.uuid}">
+                  {parent.name}
+                </a>
+              {:else}
+                <p>{parent.name}</p>
+              {/if}
+            </li>
+          {/each}
+        {/if}
         <li>
           {orgUnit.name}
         </li>
---
+++ web/src/lib/components/org/query.generated.ts
@@ -0,0 +1,13 @@
+import type * as Types from '../../graphql/types';
+
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type GetBreadcrumbsQueryVariables = Types.Exact<{
+  uuid?: Types.InputMaybe<Array<Types.Scalars['UUID']['input']> | Types.Scalars['UUID']['input']>;
+  currentDate?: Types.InputMaybe<Types.Scalars['DateTime']['input']>;
+}>;
+
+
+export type GetBreadcrumbsQuery = { org_units: { objects: Array<{ current?: { ancestors: Array<{ name: string, uuid: any }> } | null }> } };
+
+
+export const GetBreadcrumbsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetBreadcrumbs"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}},"type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"UUID"}}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"currentDate"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"DateTime"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"org_units"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"filter"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"uuids"},"value":{"kind":"Variable","name":{"kind":"Name","value":"uuid"}}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"objects"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"current"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"at"},"value":{"kind":"Variable","name":{"kind":"Name","value":"currentDate"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"ancestors"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"uuid"}}]}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetBreadcrumbsQuery, GetBreadcrumbsQueryVariables>;
