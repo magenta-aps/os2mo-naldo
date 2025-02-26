f3e08fc9f483b4450c95f8a8b3a1176d1789a499
mb <mb@magenta-aps.dk>
2025-02-26 09:54:55 +0100

    fix: [#64040] no value in user_key

--- web/src/routes/employee/[[uuid]]/create/address/+page.server.ts
+++ web/src/routes/employee/[[uuid]]/create/address/+page.server.ts
@@ -1,8 +1,10 @@
 import type { AddressCreateInput } from "$lib/graphql/types"
 import type { Actions, RequestEvent } from "@sveltejs/kit"
+import { v4 as uuidv4 } from "uuid"
 
 export const actions: Actions = {
   default: async ({ request, params }: RequestEvent): Promise<AddressCreateInput> => {
+    const addressUuid = uuidv4()
     const data = await request.formData()
     const addressType = data.get("address-type-uuid")
     const visibility = data.get("visibility")
@@ -12,9 +14,10 @@ export const actions: Actions = {
     const endDate = data.get("to")
 
     return {
+      uuid: addressUuid,
       person: params.uuid,
       address_type: addressType,
-      ...(userKey && { user_key: userKey }),
+      user_key: userKey || addressUuid,
       value: value,
       ...(visibility && { visibility: visibility }),
       validity: { from: startDate, ...(endDate && { to: endDate }) },
