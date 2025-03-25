db99beeeb60db4793c77e083147d92a4d4818c2a
mb <mb@magenta-aps.dk>
2025-03-25 13:39:13 +0100

    feat: [#64071] require uuid for terminate

--- web/src/routes/employee/[uuid]/+page.svelte
+++ web/src/routes/employee/[uuid]/+page.svelte
@@ -162,6 +162,7 @@
           { title: capital($_("date.date")), sortPath: "validity.from" },
           { title: "" },
           { title: "" },
+          { title: "" },
         ]}
       />
     {:else if activeItem === EmployeeTab.ENGAGEMENT}
