99bcd583d53ada7f03a41752089575be690e29c2
mb <mb@magenta-aps.dk>
2025-05-07 11:15:19 +0200

    feat(test): [#64956] avoid mocking

---
+++ web/tests/fixtures/snapshot.ts
@@ -0,0 +1,24 @@
+import { test as base, APIRequestContext } from "@playwright/test"
+
+export const test = base.extend<{
+  snapshot
+  moClient
+}>({
+  moClient: async ({ playwright }, use) => {
+    const request = await playwright.request.newContext({
+      baseURL: "http://localhost:5000",
+    })
+    await use(request)
+    await request.dispose()
+  },
+
+  snapshot: async ({ moClient }, use) => {
+    const snap = await moClient.post("/testing/database/snapshot")
+    if (!snap.ok()) throw new Error("Snapshot failed")
+
+    await use()
+
+    const restore = await moClient.post("/testing/database/restore")
+    if (!restore.ok()) throw new Error("Restore failed")
+  },
+})
