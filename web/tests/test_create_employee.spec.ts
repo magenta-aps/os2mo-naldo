1382c203645fab9ff85bdc9b5c70b00cf35c140a
mb <mb@magenta-aps.dk>
2025-05-07 11:15:19 +0200

    feat(test): [#64956] avoid mocking

--- web/tests/test_create_employee.spec.ts
+++ web/tests/test_create_employee.spec.ts
@@ -1,45 +1,8 @@
-import { test, expect } from "@playwright/test"
+import { test } from "./fixtures/snapshot.ts"
+import { expect } from "@playwright/test"
 import { v4 as uuidv4 } from "uuid"
 
-test("should create employee mock", async ({ page }) => {
-  // Intercept GraphQL POST requests
-  await page.route("**/graphql/**", async (route, request) => {
-    const postData = await request.postDataJSON()
-
-    // Match the mutation name and check that the values are sent in the request
-    if (postData.operationName === "CreateEmployee") {
-      const input = postData.variables.input
-
-      // Log to see the payload being sent
-      // console.log('Intercepted GraphQL mutation:', postData)
-
-      // Ensure the values from the form are correctly passed in the mutation
-      expect(input.cpr_number).toBe("1234567890")
-      expect(input.given_name).toBe("first")
-      expect(input.surname).toBe("last")
-      expect(input.nickname_given_name).toBe("nickname-first")
-      expect(input.nickname_surname).toBe("nickname-last")
-
-      // Respond with a mocked response matching the real API response
-      return route.fulfill({
-        status: 200,
-        contentType: "application/json",
-        body: JSON.stringify({
-          data: {
-            employee_create: {
-              current: {
-                name: "first last",
-                uuid: uuidv4(),
-              },
-            },
-          },
-        }),
-      })
-    }
-
-    return route.continue()
-  })
-
+test("should create employee", async ({ page, moClient, snapshot }) => {
   // Visit the page
   await page.goto("http://localhost:5173/")
 
@@ -59,7 +22,10 @@ test("should create employee mock", async ({ page }) => {
   // Submit the form
   await page.getByRole("button", { name: "Create employee" }).click()
 
-  await page.waitForResponse(
+  const graphqlResponse = await page.waitForResponse(
     (response) => response.url().includes("/graphql") && response.status() === 200
   )
+
+  const response = await graphqlResponse.json()
+  expect(response).toHaveProperty("data.employee_create.current.name", "first last")
 })
