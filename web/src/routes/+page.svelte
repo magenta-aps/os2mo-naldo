4e269bba9b7f4a15afe0a7ed0e5fba38b8ab38f8
mb <mb@magenta-aps.dk>
2025-05-13 10:58:23 +0200

    fix: [#64071] center image + fix resize while scroll visible

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -7,19 +7,18 @@
   <title>OS2mo</title>
 </svelte:head>
 
-<div class="grid grid-cols-1 px-4 xl:px-12 pt-10 gap-4 xl:gap-20">
+<div class="grid grid-cols-1 px-4 xl:px-12 pt-10 gap-4 xl:gap-20 overflow-hidden">
   <!-- Main Content with Welcome Message and Image -->
-  <div>
-    <h1 class="mb-2 text-primary text-center">
-      {$_("welcome_message")}
-    </h1>
-    <h3 class="mb-4 text-secondary text-center font-bold">
-      {$_("mo")}
-    </h3>
-    <img
-      class="mx-auto max-w-screen-sm"
-      src={indexPageImage}
-      alt={$_("welcome_message")}
-    />
+  <div class="flex justify-center w-full">
+    <div class="w-full max-w-screen-lg -ml-[386px]">
+      <!-- Adjust 160px based on the sidebar width -->
+      <h1 class="mb-2 text-primary text-center">
+        {$_("welcome_message")}
+      </h1>
+      <h3 class="mb-4 text-secondary text-center font-bold">
+        {$_("mo")}
+      </h3>
+      <img class="mx-auto max-w-xl" src={indexPageImage} alt={$_("welcome_message")} />
+    </div>
   </div>
 </div>
