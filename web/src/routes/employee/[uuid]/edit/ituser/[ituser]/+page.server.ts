b6c1c13ffe2b757bb13f234e462ea34ab60253f0
mb <mb@magenta-aps.dk>
2025-10-27 11:52:51 +0100

    feat: [#65411] align ituser forms to update according to startDate + remove checkbox

--- web/src/routes/employee/[uuid]/edit/ituser/[ituser]/+page.server.ts
+++ web/src/routes/employee/[uuid]/edit/ituser/[ituser]/+page.server.ts
@@ -6,14 +6,14 @@ export const actions: Actions = {
     const data = await request.formData()
     const itSystem = data.get("it-system")
     const accountName = data.get("account-name") as string
-    const primary = data.get("primary") ? data.get("primary") : data.get("non-primary")
+    const primary = data.get("primary")
     const notes = data.get("notes") as string
     const startDate = data.get("from")
     const endDate = data.get("to")
 
     return {
       uuid: params.ituser,
-      primary: primary,
+      ...(primary && { primary: primary }),
       ...(accountName && { user_key: accountName }),
       ...(itSystem && { itsystem: itSystem }),
       ...(notes && { note: notes }),
