6cb4ee27c5815084a7cf7b5a257cd2403996d410
mb <mb@magenta-aps.dk>
2025-10-30 09:17:17 +0100

    feat: [#65411] align leave forms to update according to startDate

--- web/src/routes/employee/[uuid]/edit/leave/[leave]/+page.svelte
+++ web/src/routes/employee/[uuid]/edit/leave/[leave]/+page.svelte
@@ -7,7 +7,6 @@
   import Button from "$lib/components/shared/Button.svelte"
   import { enhance } from "$app/forms"
   import type { SubmitFunction } from "./$types"
-  import { goto } from "$app/navigation"
   import { base } from "$app/paths"
   import { success, error } from "$lib/stores/alert"
   import { graphQLClient } from "$lib/http/client"
@@ -15,16 +14,30 @@
   import { page } from "$app/stores"
   import { date } from "$lib/stores/date"
   import { filterClassesByFacetUserKey } from "$lib/utils/classes"
-  import { LeaveAndFacetDocument, UpdateLeaveDocument } from "./query.generated"
-  import { formatEngagementTitlesAndUuid } from "$lib/utils/helpers"
+  import {
+    LeaveAndFacetDocument,
+    GetEngagementsDocument,
+    UpdateLeaveDocument,
+  } from "./query.generated"
+  import {
+    formatEngagementTitlesAndUuid,
+    type EngagementTitleAndUuid,
+  } from "$lib/utils/helpers"
+  import type { FacetValidities } from "$lib/utils/classes"
   import { getEngagementValidities } from "$lib/http/getValidities"
+  import { getClasses } from "$lib/http/getClasses"
   import { form, field } from "svelte-forms"
   import { required } from "svelte-forms/validators"
   import Search from "$lib/components/search/Search.svelte"
   import Skeleton from "$lib/components/forms/shared/Skeleton.svelte"
 
+  let startDate: string = $date
   let toDate: string
 
+  let selectedPerson: {
+    uuid: string
+    name: string
+  }
   let selectedEngagement: {
     uuid: string
     name: string
@@ -36,25 +49,7 @@
   const svelteForm = form(fromDate, leaveType, engagement)
 
   gql`
-    query LeaveAndFacet(
-      $uuid: [UUID!]
-      $fromDate: DateTime
-      $toDate: DateTime
-      $currentDate: DateTime
-    ) {
-      facets(filter: { user_keys: ["leave_type"] }) {
-        objects {
-          validities {
-            uuid
-            user_key
-            classes(filter: { from_date: $currentDate }) {
-              uuid
-              user_key
-              name
-            }
-          }
-        }
-      }
+    query LeaveAndFacet($uuid: [UUID!], $fromDate: DateTime, $toDate: DateTime) {
       leaves(filter: { uuids: $uuid, from_date: $fromDate, to_date: $toDate }) {
         objects {
           validities {
@@ -95,6 +90,30 @@
                   name
                 }
               }
+              validity {
+                from
+                to
+              }
+            }
+          }
+        }
+      }
+    }
+
+    query GetEngagements($uuid: [UUID!], $fromDate: DateTime, $toDate: DateTime) {
+      engagements(
+        filter: { employees: $uuid, from_date: $fromDate, to_date: $toDate }
+      ) {
+        objects {
+          validities {
+            org_unit(filter: { from_date: $fromDate, to_date: $toDate }) {
+              name
+              user_key
+            }
+            uuid
+            job_function {
+              user_key
+              name
             }
           }
         }
@@ -111,20 +130,6 @@
       }
     }
   `
-
-  // Logic for updating datepicker intervals
-  let validities: {
-    from: string | undefined | null
-    to: string | undefined | null
-  } = { from: null, to: null }
-
-  $: if (selectedEngagement) {
-    ;(async () => {
-      validities = await getEngagementValidities(selectedEngagement.uuid)
-    })()
-  } else {
-    validities = { from: null, to: null }
-  }
 
   const handler: SubmitFunction =
     () =>
@@ -156,6 +161,59 @@
         }
       }
     }
+
+  const updateEngagements = async (
+    employeeUuid: string | undefined | null,
+    fromDate: string,
+    toDate: string
+  ) => {
+    const res = await graphQLClient().request(GetEngagementsDocument, {
+      uuid: employeeUuid,
+      fromDate: fromDate,
+      toDate: toDate,
+    })
+    return (engagements = res.engagements?.objects.map((e) => e.validities[0]))
+  }
+
+  // Logic for updating datepicker intervals
+  let validities: {
+    from: string | undefined | null
+    to: string | undefined | null
+  } = { from: null, to: null }
+
+  $: if (selectedEngagement) {
+    ;(async () => {
+      validities = await getEngagementValidities(selectedEngagement.uuid)
+    })()
+  } else {
+    validities = { from: null, to: null }
+  }
+
+  let facets: FacetValidities[]
+  let engagements: EngagementTitleAndUuid[]
+  let abortController: AbortController
+  $: {
+    // Abort the previous request if a new one is about to start
+    if (abortController) abortController.abort()
+    abortController = new AbortController()
+
+    const params = {
+      currentDate: startDate,
+      orgUuid: null,
+      facetUserKeys: ["leave_type"],
+    }
+
+    ;(async () => {
+      try {
+        facets = await getClasses(params, abortController.signal)
+      } catch (err: any) {
+        if (err.name !== "AbortError") {
+          console.error("Request failed:", err)
+        }
+      }
+      await updateEngagements($page.params.uuid, startDate, toDate)
+    })()
+  }
 </script>
 
 <title
@@ -178,7 +236,7 @@
 
 <div class="divider p-0 m-0 mb-4 w-full" />
 
-{#await graphQLClient().request( LeaveAndFacetDocument, { uuid: $page.params.leave, fromDate: $page.url.searchParams.get("from"), toDate: $page.url.searchParams.get("to"), currentDate: $date } )}
+{#await graphQLClient().request( LeaveAndFacetDocument, { uuid: $page.params.leave, fromDate: $page.url.searchParams.get("from"), toDate: $page.url.searchParams.get("to") } )}
   <div class="mx-6">
     <div class="sm:w-full md:w-3/4 xl:w-1/2 bg-slate-100 rounded">
       <div class="p-8">
@@ -194,9 +252,7 @@
   </div>
 {:then data}
   {@const leave = data.leaves.objects[0].validities[0]}
-  {@const facets = data.facets.objects}
-  {@const engagements = leave.person[0].engagements}
-  {@const employee = leave.person[0]}
+  {@const person = leave.person[0]}
   {@const engagementStartValue = formatEngagementTitlesAndUuid([leave.engagement])[0]}
 
   <form method="post" class="mx-6" use:enhance={handler}>
@@ -204,8 +260,8 @@
       <div class="p-8">
         <div class="flex flex-row gap-6">
           <DateInput
-            startValue={$date}
-            bind:value={$fromDate.value}
+            bind:value={startDate}
+            bind:validationValue={$fromDate.value}
             errors={$fromDate.errors}
             title={capital($_("date.start_date"))}
             id="from"
@@ -223,34 +279,40 @@
           />
         </div>
 
-        <Select
-          startValue={leave.leave_type}
-          title={capital($_("leave_type"))}
-          id="leave-type-uuid"
-          bind:name={$leaveType.value}
-          errors={$leaveType.errors}
-          iterable={filterClassesByFacetUserKey(facets, "leave_type")}
-          required={true}
-        />
         <Search
           type="employee"
+          bind:value={selectedPerson}
           startValue={{
-            uuid: employee.uuid,
-            name: employee.name,
+            uuid: person.uuid,
+            name: person.name,
           }}
           disabled
           required={true}
         />
-        <Select
-          title={capital($_("engagement", { values: { n: 2 } }))}
-          id="engagement-uuid"
-          startValue={engagementStartValue}
-          bind:value={selectedEngagement}
-          bind:name={$engagement.value}
-          errors={$engagement.errors}
-          iterable={formatEngagementTitlesAndUuid(engagements)}
-          required={true}
-        />
+        {#if facets}
+          <Select
+            startValue={leave.leave_type}
+            title={capital($_("leave_type"))}
+            id="leave-type-uuid"
+            bind:name={$leaveType.value}
+            errors={$leaveType.errors}
+            iterable={filterClassesByFacetUserKey(facets, "leave_type")}
+            required={true}
+          />
+        {/if}
+
+        {#if engagements && engagements.length}
+          <Select
+            title={capital($_("engagement", { values: { n: 2 } }))}
+            id="engagement-uuid"
+            startValue={engagementStartValue}
+            bind:value={selectedEngagement}
+            bind:name={$engagement.value}
+            errors={$engagement.errors}
+            iterable={formatEngagementTitlesAndUuid(engagements)}
+            required={true}
+          />
+        {/if}
       </div>
     </div>
     <div class="flex py-6 gap-4">
