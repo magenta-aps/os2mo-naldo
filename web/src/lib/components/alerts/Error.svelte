291ca97e473c442856f93a71027339292455c9a7
mb <mb@magenta-aps.dk>
2025-11-20 16:23:40 +0100

    feat: [#67618] translate MO errors

--- web/src/lib/components/alerts/Error.svelte
+++ web/src/lib/components/alerts/Error.svelte
@@ -5,26 +5,25 @@
   import Icon from "@iconify/svelte"
   import errorCircleRoundedOutline from "@iconify/icons-material-symbols/error-circle-rounded-outline"
 
-  let errorMessage = ""
+  let errorKey = ""
 
   $: if ($error.message) {
-    console.error($error.message)
-    errorMessage = $error.message.response.errors.extensions
-      ? $error.message.response.errors[0].extensions.error_context.description
-      : $error.message.response.errors[0].message
-    setTimeout(() => (errorMessage = ""), 5000)
+    const err = $error.message.response.errors?.[0]
+    console.error(err)
+    errorKey = err.extensions?.error_context?.error_key ?? err.message
+    setTimeout(() => (errorKey = ""), 5000)
   }
   $: if ($success) {
-    errorMessage = ""
+    errorKey = ""
   }
 </script>
 
-{#if errorMessage}
+{#if errorKey}
   <div class="toast toast-end" transition:slide>
     <div class="alert alert-error shadow-lg">
       <div class="flex gap-1">
         <Icon icon={errorCircleRoundedOutline} width="20" height="20" />
-        <span>{errorMessage}</span>
+        <span>{$_(errorKey)}</span>
       </div>
     </div>
   </div>
