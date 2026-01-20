9996681a528cfe36b89894e4ae975bce83087def
mb <mb@magenta-aps.dk>
2026-01-20 10:45:05 +0100

    feat: [#67350] bump

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,36 +8,4 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    require('daisyui')
-  ],
-  daisyui: {
-    themes: [{
-      magenta: {
-
-        // Primary
-        primary: "#1053AB",
-        secondary: "#00244E",
-        accent: "#D1E4FF", // Also know as tertiary
-        neutral: "#121212",
-        "base-100": "#FFFFFF",
-
-        // Neutral - Doesn't overwrite, just for reference
-        "slate-100": "#F8FAFC",
-        "slate-300": "#CBD5E1",
-        "slate-600": "#475569",
-
-        // Text
-        "primary-text": "#00244E",
-        "secondary-text": "#475569",
-        "tertiary-text": "#CBD5E1",
-
-        // Accent
-        error: "#DC3545",
-        success: "#368632",
-        info: "#1053AB",
-        warning: "#FBBD23",
-      }
-    }],
-  }
 }
