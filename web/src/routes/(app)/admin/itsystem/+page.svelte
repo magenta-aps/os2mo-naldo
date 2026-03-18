cdb3c879e3e851f3e39195ecb8fbb4035bf1f4e5
mb <mb@magenta-aps.dk>
2026-03-18 11:55:12 +0100

    fix: [#69261] fix description + dynamic button

--- web/src/routes/(app)/admin/itsystem/+page.svelte
+++ web/src/routes/(app)/admin/itsystem/+page.svelte
@@ -32,9 +32,6 @@
   <main class="flex-1">
     <h1 class="pb-4">
       {capital($_("itsystem", { values: { n: 2 } }))}
-      <!-- <div class="tooltip tooltip-bottom" data-tip={$_("classifications_text")}> -->
-      <!--   <Icon class="align-middle" icon={infoOutlineRounded} width="25" height="25" /> -->
-      <!-- </div> -->
     </h1>
 
     {#if roleFacet}
