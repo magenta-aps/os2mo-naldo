b573d983c00a215537bf038511c9c4c1d0ad2d9c
Miklas Bøgvald <mb@magenta-aps.dk>
2026-06-26 07:46:47 +0000

    refactor: [#xxxxx] rewrite employee edit engagement form to idiomatic Svelte

--- web/src/routes/(app)/employee/[uuid]/edit/engagement/[engagement]/+page.svelte
+++ web/src/routes/(app)/employee/[uuid]/edit/engagement/[engagement]/+page.svelte
@@ -22,6 +22,7 @@
   import { required } from "svelte-forms/validators"
   import Breadcrumbs from "$lib/components/org/Breadcrumbs.svelte"
   import Skeleton from "$lib/components/forms/shared/Skeleton.svelte"
+  import { onDestroy } from "svelte"
   import { getValidities } from "$lib/http/getValidities"
   import { getClasses } from "$lib/http/getClasses"
 
@@ -143,33 +144,27 @@
     to: string | undefined | null
   } = { from: null, to: null }
 
-  let facets: FacetValidities[]
-  let abortController: AbortController
-  $: {
-    // Abort the previous request if a new one is about to start
-    if (abortController) abortController.abort()
-    abortController = new AbortController()
-
-    // Make sure `currentDate` isn't sent if startDate is null.
-    const params = {
-      currentDate: startDate,
-      orgUuid: selectedOrgUnit?.uuid,
-      facetUserKeys: ["engagement_type", "engagement_job_function", "primary_type"],
-    }
-
-    ;(async () => {
-      validities = selectedOrgUnit
-        ? await getValidities(selectedOrgUnit.uuid)
-        : { from: null, to: null }
-      try {
-        facets = await getClasses(params, abortController.signal)
-      } catch (err: any) {
-        if (err.name !== "AbortError") {
-          console.error("Request failed:", err)
-        }
-      }
-    })()
+  $: if (selectedOrgUnit?.uuid) {
+    getValidities(selectedOrgUnit.uuid).then((v) => { validities = v })
+  } else {
+    validities = { from: null, to: null }
   }
+
+  let facetsController: AbortController
+  onDestroy(() => facetsController?.abort())
+
+  $: facetsPromise = (() => {
+    facetsController?.abort()
+    facetsController = new AbortController()
+    return getClasses(
+      {
+        currentDate: startDate,
+        orgUuid: selectedOrgUnit?.uuid,
+        facetUserKeys: ["engagement_type", "engagement_job_function", "primary_type"],
+      },
+      facetsController.signal
+    )
+  })()
 
   let initialEngagement: any = null
   let hasChanges = false
@@ -279,7 +274,7 @@
           required={true}
         />
         <Breadcrumbs orgUnit={selectedOrgUnit} />
-        {#if facets}
+        {#await facetsPromise then facets}
           <div class="flex flex-row gap-6">
             <Input
               title="ID"
@@ -354,7 +349,9 @@
               isClearable={true}
             />
           </div>
-        {/if}
+        {:catch}
+          <p class="text-sm text-error">{capital($_("load_error"))}</p>
+        {/await}
       </div>
     </div>
     <div class="flex py-6 gap-4">
