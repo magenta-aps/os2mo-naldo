2413fc8151f143c063fba17d219eabab893c1be6
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2026-06-29 14:54:08 +0200

    feat: [#58898] add policies information

--- web/src/routes/(app)/admin/policies/+page.svelte
+++ web/src/routes/(app)/admin/policies/+page.svelte
@@ -2,6 +2,8 @@
   import { _ } from "svelte-i18n"
   import { capital } from "$lib/utils/helpers"
   import HeadTitle from "$lib/components/shared/HeadTitle.svelte"
+  import Icon from "@iconify/svelte"
+  import infoOutlineRounded from "@iconify/icons-material-symbols/info-outline-rounded"
 </script>
 
 <HeadTitle type="admin" />
@@ -10,6 +12,9 @@
   <main class="flex-1">
     <h1 class="pb-4">
       {capital($_("policies", { values: { n: 2 } }))}
+      <div class="tooltip tooltip-bottom" data-tip={$_("policies_text")}>
+        <Icon class="align-middle" icon={infoOutlineRounded} width="25" height="25" />
+      </div>
     </h1>
   </main>
 </div>
