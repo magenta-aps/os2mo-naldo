e8aa41b2533d9ffa679eb7d5173ab549bd4695dc
Miklas Bøgvald <mb@magenta-aps.dk>
2026-08-05 10:26:01 +0000

    refactor: [#67974] split formatITUserITSystemName into singular and plural

--- web/src/routes/(app)/organisation/[[uuid]]/create/rolebinding/+page.svelte
+++ web/src/routes/(app)/organisation/[[uuid]]/create/rolebinding/+page.svelte
@@ -23,7 +23,7 @@
   import { form, field } from "svelte-forms"
   import { required } from "svelte-forms/validators"
   import { filterClassesByFacetUserKey } from "$lib/utils/classes"
-  import { formatITUserITSystemName } from "$lib/utils/helpers"
+  import { formatITUserITSystemNames } from "$lib/utils/helpers"
   import removeRounded from "@iconify/icons-material-symbols/remove-rounded"
   import addRounded from "@iconify/icons-material-symbols/add-rounded"
 
@@ -146,7 +146,7 @@
         fromDate: startDate,
       })
       return (
-        formatITUserITSystemName(
+        formatITUserITSystemNames(
           res.org_units.objects[0]?.validities?.flatMap((v) => v.itusers ?? [])
         ) ?? []
       )
